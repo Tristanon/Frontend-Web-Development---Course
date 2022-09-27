e00aa22c6fc951842867116eeeaf30ca9ab5e274
Tri Ho <106542253+Tristanon@users.noreply.github.com>
2022-09-27 15:39:44 -0600

    Create Firstday.js

---
+++ Firstday.js
@@ -0,0 +1,11 @@
+const myBoolean = true;
+const mystring = 'Hello world'; 
+const firstNumber = 20; 
+let secondNumber = 40;
+secondNumber = 80;
+myArray = [myBoolean,mystring];
+myObject = {firstProperty:myArray,sumProperty:firstNumber + secondNumber};
+myObject.sumProperty;
+myObject.firstProperty;
+myObject.firstProperty[1];
+
