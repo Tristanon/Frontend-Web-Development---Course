0f4acf48d5732c3c14d0be300cce1a3fddafbe3c
Tri Ho <106542253+Tristanon@users.noreply.github.com>
2022-09-29 11:59:44 -0600

    Create challenge2.js

---
+++ challenge2.js
@@ -0,0 +1,13 @@
+// ===== SNIPPIT 1 =====
+const firstNumber = 20;
+const secondNumber = "20";
+
+const result = firstNumber === secondNumber;
+// ===== SNIPPIT 1 ===== 
+// ===== SNIPPIT 2 =====
+const firstNumber = 20;
+const secondNumber = "20";
+
+const result = firstNumber === secondNumber;
+// ===== SNIPPIT 2 ===== 
+// result of SNIPPIT 1 and SNIPPIT 2 is the same.
