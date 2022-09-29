45c3a826c92bd4da2e5ec1907cb5484a14d77094
Tri Ho <106542253+Tristanon@users.noreply.github.com>
2022-09-29 13:24:17 -0600

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,12 @@
+//Error:
+const NumberVariable = 0;
+
+NumberVariable++;
+NumberVariable++;
+NumberVariable++;
+
+console.log(NumberVariable); 
+//Fix:
 let NumberVariable = 0;
 
 NumberVariable++;
