8df26122cf4ed329052be39ba293ee70b77c0e05
Tri Ho <106542253+Tristanon@users.noreply.github.com>
2022-10-01 12:56:13 -0600

    Update GetPlanetNameByID.py

--- GetPlanetNameByID.py
+++ GetPlanetNameByID.py
@@ -1,3 +1,5 @@
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+#Solution 1:
 function getPlanetName(id){
   var name;
   switch(id){
@@ -26,4 +28,38 @@ function getPlanetName(id){
       name = 'Neptune'
       return name;
   }
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+#Solution 2:
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break;
+    case 2:
+      name = 'Venus'
+      break;
+    case 3:
+      name = 'Earth'
+      break;
+    case 4:
+      name = 'Mars'
+      break;
+    case 5:
+      name = 'Jupiter'
+      break;
+    case 6:
+      name = 'Saturn'
+      break;
+    case 7:
+      name = 'Uranus'
+      break;
+    case 8:
+      name = 'Neptune'
+      break;
+  }
+  
+  return name;
+  //----------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 }
