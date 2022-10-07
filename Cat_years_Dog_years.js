2b7f3dc650f0342faab0c8f6b75e040889cbb3fa
Tri Ho <106542253+Tristanon@users.noreply.github.com>
2022-10-06 23:11:11 -0600

    Update Cat_years_Dog_years.js

--- Cat_years_Dog_years.js
+++ Cat_years_Dog_years.js
@@ -23,3 +23,10 @@ var humanYearsCatYearsDogYears = function(humanYears) {
 }
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //Solution 2:
+var humanYearsCatYearsDogYears = function(humanYears) {
+  if (humanYears == 0){ return [0,0,0]}
+  if (humanYears == 1){ return [1,15,15]}
+  if (humanYears == 2){ return [2,24,24]}
+  if (humanYears > 2){ return [humanYears,24+4*(humanYears-2),24+5*(humanYears-2)]}
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------//
